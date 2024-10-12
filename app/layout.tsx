6b0b4cada947c5f1b6e0f408e1991cd1da589e1a
tacheraSasi <141908385+tacheraSasi@users.noreply.github.com>
2024-10-12 16:48:31 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import { Metadata } from "next";
+import { Toaster } from "sonner";
+
+import { Navbar } from "@/components/custom/navbar";
+import { ThemeProvider } from "@/components/custom/theme-provider";
+
+import "./globals.css";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://gemini.vercel.ai"),
+  title: "Next.js Gemini Chatbot",
+  description: "Next.js chatbot template using the AI SDK and Gemini.",
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="antialiased">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Toaster position="top-center" />
+          <Navbar />
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}

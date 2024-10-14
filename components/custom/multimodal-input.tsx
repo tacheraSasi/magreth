b772678c34eb7fcfa2a94451e0d4fca97aded761
tacheraSasi <tacherasasi@gmail.com>
2024-10-15 01:31:11 +0300

    changes on the playSound

--- components/custom/multimodal-input.tsx
+++ components/custom/multimodal-input.tsx
@@ -244,9 +244,9 @@ export function MultimodalInput({
         <Button
           className="rounded-full  p-1.5 h-fit absolute bottom-2 right-2 m-0.5 text-white"
           onClick={(event) => {
-            handleSubmit(event);
-            vibrate(100);
             playSound("/sounds/out.mp3") //playing the message outgoing sound
+            vibrate(50);
+            handleSubmit(event);
           }}
           disabled={input.length === 0 || uploadQueue.length > 0}
         >

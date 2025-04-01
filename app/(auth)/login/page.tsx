ac9b820b92a898ca656d0aaa432f925aed1ba448
Tachera Sasi <tacherasasi@gmail.com>
2025-04-01 15:54:26 +0300

    Import user queries and fetch users

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -42,17 +42,17 @@ export default function Page() {
       <div className="w-full max-w-md overflow-hidden rounded-2xl flex flex-col gap-12">
         <div className="flex flex-col items-center justify-center gap-2 px-4 text-center sm:px-16">
           <h3 className="text-xl font-semibold dark:text-zinc-50">Sign In</h3>
-          <p className="text-sm text-gray-500 dark:text-zinc-400">
+          <p className="text-sm text-neutral-500 dark:text-zinc-400">
             Use your email and password to sign in
           </p>
         </div>
         <AuthForm action={handleSubmit} defaultEmail={email}>
           <SubmitButton>Sign in</SubmitButton>
-          <p className="text-center text-sm text-gray-600 mt-4 dark:text-zinc-400">
+          <p className="text-center text-sm text-neutral-600 mt-4 dark:text-zinc-400">
             {"Don't have an account? "}
             <Link
               href="/register"
-              className="font-semibold text-gray-800 hover:underline dark:text-zinc-200"
+              className="font-semibold text-neutral-800 hover:underline dark:text-zinc-200"
             >
               Sign up
             </Link>
--- app/console/page.tsx
+++ app/console/page.tsx
@@ -1,3 +1,5 @@
+import { getUser, getUsers } from "@/db/queries"
+
 export default function ConsolePage(){
   return (
     <div></div>
